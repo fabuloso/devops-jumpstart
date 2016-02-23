7145d09f9e00d4060f19085eebba068d96fbf770
Alessandro Dalfovo <dalfovo.alessandro@gmail.com>
2016-02-23 16:12:02 +0100

    Add empty recipe for database

---
+++ cookbooks/blog/recipes/db.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: blog
+# Recipe:: db
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
--- cookbooks/blog/recipes/default.rb
+++ cookbooks/blog/recipes/default.rb
@@ -1,2 +1,3 @@
 include_recipe 'blog::web'
 include_recipe 'blog::ruby'
+include_recipe 'blog::db'
