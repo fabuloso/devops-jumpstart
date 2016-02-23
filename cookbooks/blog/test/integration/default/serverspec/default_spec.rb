c62f2de64337c49557030af40f1fb38fb5e96fe4
Alessandro Dalfovo <alessandro@MacBook-Pro-di-Alessandro.local>
2016-02-23 14:37:40 +0100

    Enable nginx service

--- cookbooks/blog/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/blog/test/integration/default/serverspec/default_spec.rb
@@ -4,4 +4,10 @@ describe 'blog::default' do
   describe package('nginx') do
       it { should be_installed }
   end
+
+  describe service('nginx') do
+    it { should be_running }
+    it { should be_enabled }
+  end
+
 end
