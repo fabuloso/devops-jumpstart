3ae7b11df09f21f395db9fe8ae8d8b2d26c5af91
Alessandro Dalfovo <dalfovo.alessandro@gmail.com>
2016-02-23 15:28:24 +0100

    Refactor on files

--- cookbooks/blog/recipes/default.rb
+++ cookbooks/blog/recipes/default.rb
@@ -1,18 +1,2 @@
-package 'nginx'
-
-cookbook_file '/etc/nginx/sites-available/blog.com' do
-  source 'blog.com'
-  mode 00644
-end
-
-link '/etc/nginx/sites-enabled/blog.com' do
-  to '/etc/nginx/sites-available/blog.com'
-end
-
-file '/etc/nginx/sites-enabled/default' do
-  action :delete
-end
-
-service 'nginx' do
-  action [ :enable, :start ]
-end
+include_recipe 'blog::web'
+include_recipe 'blog::ruby'
--- cookbooks/blog/test/integration/default/serverspec/web_spec.rb
+++ cookbooks/blog/test/integration/default/serverspec/web_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'blog::default' do
+describe 'blog::web' do
   describe package('nginx') do
       it { should be_installed }
   end
